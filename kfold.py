2ade1a01fa893fbdc9a434e853b7781eb2241509
My-Linh Tang <135262874+mylinhtang@users.noreply.github.com>
2024-06-28 00:14:43 -0400

    Update kfold.py

--- kfold.py
+++ kfold.py
@@ -20,7 +20,7 @@ def kfold_cross_validation(model, dataset):
     # Set random seed
     torch.manual_seed(42)
     
-    num_epochs = 2
+    num_epochs = 10
     learning_rate = 0.003
     kfold_num = 10
     batch_size = 32
